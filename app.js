0f8ac7e42cc748f5b73d9132573d4bf4d4baafc5
Axel <axelsp050@gmail.com>
2022-07-27 10:56:41 -0400

    test run

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // api accessing format
-fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=d14b4e290b074f7f90398b5e0a9a46ca&number=10&query=espresso_grinder')
-.then((beans) => beans.json())
-.then((data) => console.log(data));
+fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=d14b4e290b074f7f90398b5e0a9a46ca&number=10&query=pasta')
+    .then((beans) => beans.json())
+    .then((data) => console.log(data));
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
