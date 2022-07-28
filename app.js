099690bbb67e8d1e82482a493e9d93c0fe22ea98
Axel <axelsp050@gmail.com>
2022-07-28 15:44:27 -0400

    created an About us page, along with its own css page. also added a header with a hamburger menu that used javascript. added style to the main css.

--- app.js
+++ app.js
@@ -2,3 +2,22 @@
 fetch('https://api.spoonacular.com/food/search?apiKey=d14b4e290b074f7f90398b5e0a9a46ca&number=10&query=apple')
     .then((beans) => beans.json())
     .then((data) => console.log(data));
+
+
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top = '-100%';
+}
