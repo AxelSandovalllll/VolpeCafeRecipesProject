f366be9803c908b373b05cf71237b443b04be1d4
Axel <axelsp050@gmail.com>
2022-08-01 15:05:42 -0400

    changed title color on home and about pages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-    fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=61bba8c3712641a384c5d6d4e1665cc2&number=1&query=coffee&addRecipeInformation=true')
+fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=61bba8c3712641a384c5d6d4e1665cc2&number=1&query=coffee&addRecipeInformation=true')
     .then((beans) => beans.json())
     .then((beans) => {
         console.log(beans);
@@ -27,30 +27,9 @@
                         </div>
                     </div>      
                 </div> `)
-            document.getElementById('recipe-grid').innerHTML += item;  
+            document.getElementById('recipe-grid').innerHTML += item;
         });
     });
-    
-
-   const mainMenu = document.querySelector('.mainMenu');
-   const closeMenu = document.querySelector('.closeMenu');
-   const openMenu = document.querySelector('.openMenu');
-   
-   
-   openMenu.addEventListener('click', show);
-   closeMenu.addEventListener('click', close);
-   
-   function show() {
-       mainMenu.style.display = 'flex';
-       mainMenu.style.top = '0';
-   }
-   
-   function close() {
-       mainMenu.style.top = '-100%';
-   }
-
-const ratingStars = [...document.getElementsByClassName("rating__star")];
-
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
@@ -68,25 +47,24 @@ function close() {
     mainMenu.style.top = '-100%';
 }
 
-
-
+const ratingStars = [...document.getElementsByClassName("rating__star")];
 
 function executeRating(stars) {
-  const starClassActive = "rating__star fas fa-star";
-  const starClassInactive = "rating__star far fa-star";
-  const starsLength = stars.length;
-  let i;
-  stars.map((star) => {
-    star.onclick = () => {
-      i = stars.indexOf(star);
+    const starClassActive = "rating__star fas fa-star";
+    const starClassInactive = "rating__star far fa-star";
+    const starsLength = stars.length;
+    let i;
+    stars.map((star) => {
+        star.onclick = () => {
+            i = stars.indexOf(star);
 
-      if (star.className.indexOf(starClassInactive) !== -1) {
-        for (i; i >= 0; --i) stars[i].className = starClassActive;
-      } else {
-        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
-      }
-    };
-  });
+            if (star.className.indexOf(starClassInactive) !== -1) {
+                for (i; i >= 0; --i) stars[i].className = starClassActive;
+            } else {
+                for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+            }
+        };
+    });
 }
 
 executeRating(ratingStars);
